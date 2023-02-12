fb3fb4d371a9031db9479f24e1ef3d77a4f80439
ujjwal1103 <ujjwallade11@gmail.com>
2023-02-12 23:47:14 +0530

    update

--- src/index.js
+++ src/index.js
@@ -17,8 +17,11 @@ app.use(cors(corsOptions));
 
 
 app.get('/cors', (req, res) => {
-  res.set('Access-Control-Allow-Origin', '*');
-  res.send({ "msg": "This has CORS enabled 🎈" })
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
   })
 
   
